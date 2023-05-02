874d3ecdf96339c3d60641270c956e014843bcd3
miryam peretz <miperetz@g.jct.ac.il>
2023-05-02 14:09:15 +0300

    TESTING tube

--- unitTests/geometries/TubeTest.java
+++ unitTests/geometries/TubeTest.java
@@ -1,16 +1,43 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TubeTest {
 
-    @Test
-    void getAxisRay() {
-    }
 
+    /**
+     * Test method for {@link Tube#getNormal(Point)}.
+     */
     @Test
-    void getNormal() {
+    void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        Tube tube = new Tube(new Ray(new Point(0,0,1),new Vector(0,-1,0)),1.0);
+        Vector normal = tube.getNormal(new Point(0,8,2));
+        double check = normal.dotProduct(tube.getAxisRay().getDir());
+        boolean firstNormal = new Vector(0,0,1).equals(normal);
+        boolean secondNormal = new Vector(0,0,-1).equals(normal);
+
+
+        assertEquals(0d,check,"normal is not orthogonal to the tube");
+
+
+        assertTrue(firstNormal||secondNormal,"wrong normal to tube");
+
+        // =============== Boundary Values Tests ==================
+        normal = tube.getNormal(new Point(0,0,2));
+        check = normal.dotProduct(tube.getAxisRay().getDir());
+        firstNormal = new Vector(0,0,1).equals(normal);
+        secondNormal = new Vector(0,0,-1).equals(normal);
+
+
+        assertEquals(0d,check,"normal is not orthogonal to the tube");
+
+
+        assertTrue(firstNormal||secondNormal,"wrong normal to tube");
     }
 }
