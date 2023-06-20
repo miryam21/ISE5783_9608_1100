afc2cf9ca7c24ca9c13b01f46480ec2a9dc1ae86
miryam peretz <miperetz@g.jct.ac.il>
2023-06-20 13:01:51 +0300

    trying and trying and more trying

--- unitTests/renderer/picture.java
+++ unitTests/renderer/picture.java
@@ -27,19 +27,25 @@ public class picture {
                 .setVPSize(150, 150).setVPDistance(1000);
 
         scene.geometries.add( //
-                new Sphere(50d,new Point(0, 0, -50)).setEmission(new Color(BLUE)) //
-                        .setMaterial(new Material().setKD(0.4).setKS(0.3).setNShininess(100).setKT(0.3)),
-                new Sphere(25d,new Point(80, 0, -50)).setEmission(new Color(RED)) //
-                        .setMaterial(new Material().setKD(0.5).setKS(0.5).setNShininess(100)));
-
-        //scene.lights.add( //
-                //new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
-                          //.setkL(0.0004).setkQ(0.0000006));
+//                new Sphere(50d,new Point(0, 0, -50)).setEmission(new Color(BLUE)) //
+//                       .setMaterial(new Material().setKD(0.4).setKS(0.3).setNShininess(100).setKT(0.3)),
+//                new Triangle(new Point(-50, -40, 0), new Point(-50, -70, 0), new Point(-68, -68, -4)).setEmission(new Color(YELLOW))
+//                        .setMaterial(new Material().setKD(0.4).setKS(0.3).setNShininess(100)),
+                new Polygon(new Point (-10, 50, 0) ,new Point(10, 50, 0), new Point(10, -50, 0) ,
+                        new Point(-10, -50, 0)).setEmission(new Color(GREEN)).setMaterial(new Material()
+                        .setKD(0.4).setKS(0.3).setNShininess(100)));
+//                new Polygon(new Point(-80,-10,20),new Point(-80,-80,20),
+//                        new Point(40,10,100),new Point(40,80,100)).setEmission(new Color(PINK))
+//                        .setMaterial(new Material().setKD(0.4).setKS(0.3).setNShininess(100)),
+//                new Sphere(25d,new Point(80, 0, -50)).setEmission(new Color(RED)) //
+//                        .setMaterial(new Material().setKD(0.5).setKS(0.5).setNShininess(100)));
+//        scene.lights.add( //
+//                new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
+//                        .setkL(0.0004).setkQ(0.0000006));
 
         camera.setImageWriter(new ImageWriter("picture", 500, 500)) //
                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage(); //
-        camera .writeToImage();
-    }
+        camera .writeToImage();}
 
 }
