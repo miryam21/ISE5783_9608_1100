bf187b806f1001cf52e48335992d1c5722cfa88f
shirel taieb <shitaieb@g.jct.ac.il>
2023-06-20 11:22:59 +0300

    keep going

--- unitTests/renderer/picture.java
+++ unitTests/renderer/picture.java
@@ -21,17 +21,19 @@ public class picture {
     /** Produce a picture of a sphere lighted by a spot light */
     @Test
     public void twoSpheres() {
-        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+        //  משנה נקודת מבט מרחקים וכו
+        Camera camera = new Camera(new Point(0, 0, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setVPSize(150, 150).setVPDistance(1000);
 
         scene.geometries.add( //
                 new Sphere(50d,new Point(0, 0, -50)).setEmission(new Color(BLUE)) //
                         .setMaterial(new Material().setKD(0.4).setKS(0.3).setNShininess(100).setKT(0.3)),
-                new Sphere(25d,new Point(0, 0, -50)).setEmission(new Color(RED)) //
+                new Sphere(25d,new Point(80, 0, -50)).setEmission(new Color(RED)) //
                         .setMaterial(new Material().setKD(0.5).setKS(0.5).setNShininess(100)));
-        scene.lights.add( //
-                new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
-                        .setkL(0.0004).setkQ(0.0000006));
+
+        //scene.lights.add( //
+                //new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
+                          //.setkL(0.0004).setkQ(0.0000006));
 
         camera.setImageWriter(new ImageWriter("picture", 500, 500)) //
                 .setRayTracer(new RayTracerBasic(scene)) //
