ded87369adf17f189479a00cabca0b98bc124fcd
miryam peretz <miperetz@g.jct.ac.il>
2023-06-27 14:22:24 +0300

    cylinder working finally

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,94 +1,67 @@
 package geometries;
 
-import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
+import primitives.Point;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static primitives.Util.alignZero;
+import static primitives.Util.*;
 
 /**
- *A class that implements a cylinder, it is a finite pipe and therefore has a height.
- * The class inherits from Tube.
+ * The Cylinder class represents a cylinder in 3D space, which is a subclass of the Tube class.
+ *
+ * @author Maayan Amar
  */
 public class Cylinder extends Tube {
+    /**
+     * The height of the cylinder.
+     */
+    private final double height;
 
     /**
-     * height of the cylinder
+     * Constructs a new cylinder with the given height, axisRay, and radius.
+     *
+     * @param height  The height of the cylinder.
+     * @param axisRay The axisRay of the cylinder.
+     * @param radius  The radius of the cylinder.
      */
-    final private double height;
+    public Cylinder(double height, Ray axisRay, double radius) {
 
-
-    /**
-     * ctr for Cylinder
-     * @param radius
-     * @param axisRay
-     * @param height
-     */
-    public Cylinder(double radius, Ray axisRay, double height) {
-        super( axisRay,radius);
+        super(axisRay, radius);
         this.height = height;
     }
 
     /**
-     * get the height of the Cylinder
-     * @return
+     * Returns the height of the cylinder.
+     *
+     * @return The height of the cylinder.
      */
     public double getHeight() {
         return height;
     }
 
 
-    // region getNormal
     @Override
     public Vector getNormal(Point point) {
-        Vector v = axisRay.getDir();
         Point p0 = axisRay.getP0();
-        Point upperPoint = p0.add(v.scale(height));
+        Vector v = axisRay.getDir();
 
-        if (point.equals(p0) || point.equals(upperPoint) || v.dotProduct(point.subtract(p0)) == 0 || v.dotProduct(point.subtract(upperPoint)) == 0)
-            return axisRay.getDir();
-        return super.getNormal(point);
-    }
+        if (point.equals(p0))
+            return v;
 
-    //endregion
+        // projection of P-p0 on the ray:
+        Vector u = point.subtract(p0);
 
-    @Override
-    public List<GeoPoint> findGeoIntersectionsHelper(Ray ray,double maxDistance) {
-        List<GeoPoint> intersections = super.findGeoIntersectionsHelper(ray,maxDistance);
+        // distance from p0 to the o who is in from of point
+        double t = alignZero(u.dotProduct(v));
 
-        List<GeoPoint> pointList = new ArrayList<>();
+        // if the point is at a base
+        if (t == 0 || isZero(height - t))
+            return v;
 
-        if(intersections != null) {
-            for (GeoPoint geoPoint : intersections) {
-                double projection = geoPoint.point.subtract(axisRay.getP0()).dotProduct(axisRay.getDir());
-                if (alignZero(projection) > 0 && alignZero(projection - this.height) < 0)
-                    pointList.add(new GeoPoint(this,geoPoint.point));
-            }
-        }
+        //the other point on the axis facing the given point
+        Point o = p0.add(v.scale(t));
 
-        // intersect with base
-        Circle base = new Circle(axisRay.getP0(), radius, axisRay.getDir());
-        intersections = base.findGeoIntersectionsHelper(ray,maxDistance);
-        if(intersections != null)
-            pointList.add(new GeoPoint(this,intersections.get(0).point));
+        return point.subtract(o).normalize();
+}
 
-        base = new Circle(axisRay.getPoint(height), radius, axisRay.getDir());
-        intersections = base.findGeoIntersectionsHelper(ray,maxDistance);
-        if(intersections != null)
-            pointList.add(new GeoPoint(this, intersections.get(0).point));
-
-        if (pointList.size() == 0)
-            return null;
-        return pointList;
-    }
-
-    @Override
-    public String toString() {
-        return "height=" + height +
-                ", axisRay=" + axisRay +
-                ", radius=" + radius ;
-    }
 }
--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -1,64 +1,118 @@
 package geometries;
 
-import primitives.Point;
-import primitives.Ray;
-import primitives.Vector;
+import primitives.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
+
 /**
- * The Tube class represents an infinite cylinder (a tube) in Cartesian coordinate system.
- * It is defined by its axis ray (the ray that passes through the center of the tube) and radius.
+ * a class that represents a tube in the space by the axis of it's center. and it's radius
  */
-public class Tube extends RadialGeometry {
+public class Tube extends Geometry{
+    protected Ray axisRay;
+    protected double radius;
 
-    final protected Ray axisRay; // The axis ray of the tube
-
-    /**
-     * Constructs a tube with the specified axis ray.
-     *
-     * @param axisRay The axis ray of the tube.
-     * @param radius  The radius of the tube.
-     */
+    //region constructor ray and a radius
     public Tube(Ray axisRay, double radius) {
-        super(radius);
-        this.axisRay = axisRay; // Sets the axis ray of the tube
+        this.axisRay = axisRay;
+        this.radius = radius;
+    }
+    //endregion
+
+    //region toString Override
+    /**
+     * format: "Tube { axisRay=  Ray { p0 = Point : xyz = (x, y, z), dir = Vector : xyz = (x, y, z) }, radius= radius}"
+     */
+    @Override
+    public String toString() {
+        return "Tube{" +
+                "axisRay=" + axisRay +
+                ", radius=" + radius +
+                '}';
+    }
+    //endregion
+
+    //region getNormal function
+    @Override
+    public Vector getNormal(Point point) {
+        // projection of p0p vector on the axisRay
+        double t = axisRay.getDir().dotProduct(point.subtract(axisRay.getP0()));
+
+        Point projectionPointOfP = axisRay.getP0();
+        if (t != 0) {
+            projectionPointOfP = axisRay.getPoint(t);
+        }
+
+        return point.subtract(projectionPointOfP).normalize();
+    }
+    //endregion
+
+    //region findGeoIntersectionsHelper
+    @Override
+    public List<GeoPoint> findGeoIntersectionsHelper(Ray ray, double maxDistance) {
+        Point o = ray.getP0();
+        Vector d = ray.getDir();
+
+        // vectors that span a space with the axis ray
+        Vector local_z = axisRay.getDir().normalize();
+        Vector local_x;
+        try{
+            local_x = d.crossProduct(local_z).normalize();
+        } catch (IllegalArgumentException x){ // ray.dir == axisRay.dir
+            return null;
+        }
+        Vector local_y = local_z.crossProduct(local_x).normalize();
+
+        Vector w = o.subtract(axisRay.getP0());
+
+        // projection of o and d on all 3 spanning vectors
+        Vector o_local = new Vector(w.dotProduct(local_x), w.dotProduct(local_y), w.dotProduct(local_z));
+        Vector d_local = new Vector(d.dotProduct(local_x), d.dotProduct(local_y), d.dotProduct(local_z));
+
+        // parameters for the root formula
+        double a = d_local.getY() * d_local.getY();
+        double b = 2 * d_local.getY() * o_local.getY();
+        double c = o_local.getY() * o_local.getY() + o_local.getX() * o_local.getX() - radius * radius;
+
+
+        if (a == 0) {   // ray is parallel to axis ray - check the bases!!
+            return null;
+        }
+
+        // root parameters
+        double e = b * b - 4 * a * c;
+
+        // complex solution - what does it mean??
+        if (e < 0) {
+            return null;
+        }
+        e = Math.sqrt(e);
+
+        List<GeoPoint> pointList = new ArrayList<>();
+        double t1 = (-b - e) / (2 * a);
+        double t2 = (-b + e) / (2 * a);
+
+        if (alignZero(t1) > 0) {
+            pointList.add(new GeoPoint(this , ray.getPoint(t1)));
+        }
+        if (alignZero(t2) > 0) {
+            pointList.add(new GeoPoint(this , ray.getPoint(t2)));
+        }
+
+        if(pointList.size() == 0)
+            return null;
+        return pointList;
     }
 
-    /**
-     * Returns the axis ray of the tube.
-     *
-     * @return The axis ray of the tube.
-     */
     public Ray getAxisRay() {
         return axisRay;
     }
 
-    /**
-     * Returns the normal vector to the tube at the specified point.
-     *
-     * @param point The point on the tube to get the normal vector at.
-     * @return The normal vector to the tube at the specified point.
-     */
-    @Override
-    public Vector getNormal(Point point) {
-        Vector u = point.subtract(axisRay.getP0());
-        double t = axisRay.getDir().dotProduct(u);
-        if (t == 0)
-            return u;
-        Point o = axisRay.getP0().add(axisRay.getDir().scale(t));
-        return point.subtract(o);
+    public double getRadius() {
+        return radius;
     }
-
-    /**
-     * Finds the intersections between the tube and a given ray.
-     *
-     * @param ray         The ray to intersect with the tube.
-     * @param maxDistance The maximum distance to check for intersections.
-     * @return A list of GeoPoint objects representing the intersections between the tube and the ray.
-     */
-    @Override
-    protected List<GeoPoint> findGeoIntersectionsHelper(Ray ray, double maxDistance) {
-        return null;
-    }
+    //endregion
 }
